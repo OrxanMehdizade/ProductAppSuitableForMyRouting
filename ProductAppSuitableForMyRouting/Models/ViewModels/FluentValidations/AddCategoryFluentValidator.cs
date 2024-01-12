755886d71871435ced80b98b3565b707b180c6bb
Mehdi_ha01 <Mehdi_ha01@STHQ0127-06>
2024-01-12 15:52:48 +0400

    update

---
+++ ProductAppSuitableForMyRouting/Models/ViewModels/FluentValidations/AddCategoryFluentValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+
+namespace ProductAppSuitableForMyRouting.Models.ViewModels.FluentValidations
+{
+    public class AddCategoryFluentValidator : AbstractValidator<AddCategoryViewModel>
+    {
+        public AddCategoryFluentValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(50).WithMessage("Title cannot exceed 50 characters.");
+
+            RuleFor(c => c.ImageUrlCategory)
+                .NotNull().WithMessage("Image is required.")
+                .Must(checkImage).WithMessage("Invalid image file.");
+        }
+
+        private bool checkImage(IFormFile formFile)
+        {
+            if (formFile == null)
+            {
+                return false;
+            }
+
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+            var file = System.IO.Path.GetExtension(formFile.FileName);
+            return allowedExtensions.Contains(file, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ ProductAppSuitableForMyRouting/Models/ViewModels/FluentValidations/ProductUpdateFluentValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+
+namespace ProductAppSuitableForMyRouting.Models.ViewModels.FluentValidations
+{
+    public class ProductUpdateFluentValidator : AbstractValidator<ProductUpdateViewModel>
+    {
+        public ProductUpdateFluentValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(50).WithMessage("Title cannot exceed 50 characters.");
+
+            RuleFor(p => p.Description)
+                .NotEmpty().WithMessage("Description is required.")
+                .MaximumLength(200).WithMessage("Description cannot exceed 200 characters.");
+
+
+
+            RuleFor(p => p.Price)
+                .NotNull().WithMessage("Price is required.")
+                .Must(check).WithMessage("Price must be a numeric value.")
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+
+            RuleFor(p => p.ImageUrl)
+                .NotNull().WithMessage("Image is required.")
+                .Must(checkImage).WithMessage("Invalid image file.");
+        }
+        private bool check(decimal value)
+        {
+            return true;
+        }
+
+        private bool checkImage(IFormFile formFile)
+        {
+            if (formFile == null)
+            {
+                return false;
+            }
+
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+            var file = System.IO.Path.GetExtension(formFile.FileName);
+            return allowedExtensions.Contains(file, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
